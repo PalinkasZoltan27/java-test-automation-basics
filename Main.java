5798c0349a573a9625f83c5091a6cf040913995a
PalinkasZoltan27 <palinkaszoltan27@gmail.com>
2024-02-18 13:58:20 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+/* ide lehet szöveget írni több sorban
+ide is jöhet mert *-os
+
+ */
+int number = 1;
+        System.out.println(number);
+
+       int num = 128;
+    }
+}
---
+++ hazi1.java
@@ -0,0 +1,8 @@
+public class hazi1{
+    public final static String PATH = "C:\\Users";
+    public static void main(String[] args) {
+
+        System.out.println(PATH);
+
+    }
+}
